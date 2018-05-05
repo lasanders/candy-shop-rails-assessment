452b3fbfce7f7225b4cc163d31fe363188dd1621
lsivak <larasanders@gmail.com>
2018-05-05 15:02:06 +0000

    playing around with google authentication. Changing .env file and omniauth file, and routes. Still getting client_id error. Not sure what's wrong.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,49 @@ def create
 end
 end
 
+ def other_create
+    auth = request.env['omniauth.auth']
+    origin = request.env['omniauth.origin']
+    destination = origin.blank? ? root_path : origin
+    @identity = Identity.find_with_omniauth(auth)
+    @identity = Identity.create_with_omniauth(auth) if @identity.nil?
+
+    if signed_in?
+      if @identity.user == current_user
+        # Identity is already associated with this user
+        redirect_to user_path(@user), notice: "Already logged in and linked"
+      else
+       @identity.user = current_user
+        @identity.save()
+        redirect_to user_path(@user), notice: "Account was successfully linked"
+      end
+    else
+      if @identity.user.present?
+        # Identity has a user associated with it
+        self.current_user = @identity.user
+        redirect_to user_path(@user)
+      else
+        # No user associated with the identity so create a new one
+        user = User.create_with_omniauth(auth['info'])
+        @identity.user = user
+        @identity.save()
+        self.current_user = @identity.user
+        redirect_to user_path(@user), notice: "Registration successful"
+      end
+    end
+  end
+
+  def other_destroy
+    self.current_user = nil
+    redirect_to root_url, notice: "Signed out successfully"
+  end
+
+  def omniauth_failure
+    origin = request.env['omniauth.origin']
+    destination = origin.blank? ? root_path : origin
+    redirect_to destination, alert: "Connection failed"
+  end
+
 def destroy
     session[:user_id] = nil
     redirect_to root_url
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :purchases
     has_many :candies, through: :purchases
+    has_many :identities, dependent: :destroy
     validates_presence_of :taste, :cash, :appetite
     validates :taste, :cash, :appetite, numericality: { only_integer: true }
 end
