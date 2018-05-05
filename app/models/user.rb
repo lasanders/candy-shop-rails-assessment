92d50df7ae6741a7bc86434aa966f875e831d8d2
lsivak <larasanders@gmail.com>
2018-05-05 22:10:33 +0000

    I FIXED MY SIGNUP PROBLEM! woo-hoo! I was validating taste as an integer, so I changed that. and now I can save a new user. Back to my omniauth problem.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
     has_many :candies, through: :purchases
     has_many :identities, dependent: :destroy
     validates_presence_of :taste, :cash, :appetite
-    validates :taste, :cash, :appetite, numericality: { only_integer: true }
+    validates :cash, :appetite, numericality: { only_integer: true }
 end
