87997addf2565c6a8c35e6ec61be622ae918cee5
lsivak <larasanders@gmail.com>
2018-05-06 16:59:24 +0000

    deleted identity.rb. changed sessions controller to try to make it more simple. Deleted has many identities from user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,51 +12,22 @@ def create
           session[:user_id] = @user.id
         # binding.pry
       redirect_to user_path(@user)
-     else
+     elsif 
+      user_info = request.env["omniauth.auth"]
+
+    @user           = User.new
+    @user.id        = user_info["uid"]
+    @user.name      = user_info["info"]["name"]
+    @user.email     = user_info["info"]["email"]
+    @user.save
+    session[:user] = @user
+
+    redirect_to user_path(@user)
+  else
       redirect_to '/signin'
        
 end
 end
-
- def other_create
-    auth = request.env['omniauth.auth']
-    origin = request.env['omniauth.origin']
-    destination = origin.blank? ? root_path : origin
-    @identity = Identity.find_with_omniauth(auth)
-    @identity = Identity.create_with_omniauth(auth) if @identity.nil?
-
-    if logged_in?
-      if @identity.user == current_user
-        # Identity is already associated with this user
-        redirect_to user_path(@user), notice: "Already logged in and linked"
-      else
-       @identity.user = current_user
-        @identity.save()
-        redirect_to user_path(@user), notice: "Account was successfully linked"
-      end
-    else
-      if @identity.user.present?
-        # Identity has a user associated with it
-        self.current_user = @identity.user
-        redirect_to user_path(@user)
-      else
-        # No user associated with the identity so create a new one
-     
-        redirect_to '/signin', notice: "I'm sorry, an error occured. Please Sign up or try again"
-      end
-    end
-  end
-
-  def other_destroy
-    self.current_user = nil
-    redirect_to root_url, notice: "Signed out successfully"
-  end
-
-  def omniauth_failure
-    origin = request.env['omniauth.origin']
-    destination = origin.blank? ? root_path : origin
-    redirect_to destination, alert: "Connection failed"
-  end
 
 def destroy
     session[:user_id] = nil
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :purchases
     has_many :candies, through: :purchases
-    has_many :identities, dependent: :destroy
     validates_presence_of :taste, :cash, :appetite
     validates :taste, :cash, :appetite, numericality: { only_integer: true }
 end
