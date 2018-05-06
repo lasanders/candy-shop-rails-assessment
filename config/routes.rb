10dfddba633af3a83572bf875ae610229b14f383
lsivak <larasanders@gmail.com>
2018-05-06 19:02:27 +0000

    still trying to get this auth to work

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,10 @@ Rails.application.routes.draw do
    
    get '/signin', to: 'sessions#new', as: 'signin'
    post '/sessions', to: 'sessions#create'
-#   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+#   devise_for :users, controllers: { omniauth_callbacks: 'auth/google_oauth2/callback' }
 #   post 'auth/request', to:'authorization#get_authorization'
-   get '/auth/google_oauth2/callback', to: 'sessions#create'
-   post '/auth/google_oauth2/callback', to: 'sessions#create'
+   get 'users/auth/google_oauth2/callback', to: 'sessions#create'
+   post 'users/auth/google_oauth2/callback', to: 'sessions#create'
    get '/signout', to: 'sessions#destroy'
    delete '/signout', to: 'sessions#destroy'
    get '/delete', to: 'candies#destroy'
