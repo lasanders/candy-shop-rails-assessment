8e2d886c7edabf6a8483dff609976272a8c1a410
lsivak <larasanders@gmail.com>
2018-05-06 19:34:57 +0000

    going back to trial_auth branch b/c at least that worked a little.

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
    post '/sessions', to: 'sessions#create'
 #   devise_for :users, controllers: { omniauth_callbacks: 'auth/google_oauth2/callback' }
 #   post 'auth/request', to:'authorization#get_authorization'
-   get 'users/auth/google_oauth2/callback', to: 'sessions#create'
-   post 'users/auth/google_oauth2/callback', to: 'sessions#create'
+   get '/auth/google_oauth2', to: 'sessions#create'
+   post '/auth/google_oauth2', to: 'sessions#create'
    get '/signout', to: 'sessions#destroy'
    delete '/signout', to: 'sessions#destroy'
    get '/delete', to: 'candies#destroy'
