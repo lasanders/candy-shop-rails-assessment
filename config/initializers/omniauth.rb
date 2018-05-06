708d21bc9bfc2148782b1dc56d3f8a503ae3c3f7
lsivak <larasanders@gmail.com>
2018-05-06 17:39:25 +0000

    installed devise_token_auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,13 +3,19 @@
 # end
 
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
-  {
-     scope: ['email',
-    'https://www.googleapis.com/auth/gmail.modify'],
-    access_type: 'offline'
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+#   {
+#     scope: ['email',
+#     'https://www.googleapis.com/auth/gmail.modify'],
+#     access_type: 'offline'
      
-  }
+#   }
+# end
+
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, 'my Google client id', 'my Google client secret', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
 
