38d53fa41a24a4ef7c1f7b1accead3db4e1d96e0
lsivak <larasanders@gmail.com>
2018-05-11 00:04:52 +0000

    Fixed issue with user candies. Should be displaying properly in user views. still need to fix candies view.

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,10 +9,16 @@ class PurchasesController < ApplicationController
   end
   
   def create
-     @purchase = Purchase.create(:user_id => params[:user_id], :candy_id => params[:candy_id])
+      
+     @purchase = Purchase.new(:user_id => params[:user_id], :candy_id => params[:candy_id])
+    #  if @purchase.get_cavities
+    #          binding.pry
+    #      @purchase.save
      @message = @purchase.purchase_candy, @purchase.hunger
     redirect_to user_path(@purchase.user, :message => @message)
+
   end
+
   
 private
 
